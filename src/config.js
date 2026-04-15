4083c467bf7d511655da4d5f605c26b323d33d99
Romy Rose Jimmy <romybijoy@gmail.com>
2026-04-15 20:29:04 +0530

    fix(build): add vite dependency for vercel deployment

--- src/config.js
+++ src/config.js
@@ -1,8 +1,5 @@
-export const hostname = 'https://api.raiserbuddy.com'
-
-// export const hostname = "http://localhost:8085";
 
 export const appConfig = {
-  'env': import.meta.env.VITE__ENV,
-  'ip': import.meta.env.VITE__API_ENDPOINT
+  'env': import.meta.env.VITE_ENV,
+  'ip': import.meta.env.VITE_API_ENDPOINT
 }
